d52cc4e254487dc4b0b2d9e95990131de585d755
daniella-patton <59801897+daniella-patton@users.noreply.github.com>
2020-01-27 21:24:55 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,32 +1,28 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-import sys, pickle
 from sklearn.externals import joblib
 
 st.title('TennisDoctor')
 
-if len(sys.argv) > 1: # parse command-line argument if supplied (folder path)
-    folder = os.path.abspath(sys.argv[1])
-else: # otherwise use the current working directory
-    folder = os.path.abspath(os.getcwd())
- 
-st.write(folder)
+url = 'https://github.com/daniella-patton/Test2/blob/master/App_data.csv'
+df = pd.read_csv(url, index_col=0)
+print(df.head(5))
 
 # function to load model
-@st.cache # cache
-def load_csv(modelName):
-	model = pd.read_csv(os.path.join(folder, modelName + '.csv'))
-	return model
+#@st.cache # cache
+#def load_csv(modelName):
+#	model = pd.read_csv(os.path.join(folder, modelName + '.csv'))
+#	return model
         
 #@st.cache
 #def get_data():
 #    url = "https://github.com/daniella-patton/Test2/blob/master/App_data.csv"
 #    return pd.read_csv(url, error_bad_lines=False)
 
-modelName = 'App_data'
-master = load_csv(modelName)
-st.write(master)
-#unique_names = master.PlayerName.unique()
+#modelName = 'App_data'
+#master = load_csv(modelName)
+st.write(df)
+unique_names = master.PlayerName.unique()
 
 
