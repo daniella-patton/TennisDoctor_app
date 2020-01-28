4b60b4f7abd56771c5c561f761058bc042fe78fa
daniella-patton <59801897+daniella-patton@users.noreply.github.com>
2020-01-27 19:36:40 -0500

    Update app.py

--- app.py
+++ app.py
@@ -12,17 +12,5 @@ def get_data():
 
 master = get_data()
 unique_names = master.PlayerName.unique()
-# Load in the model 
-# load the model from disk
-#filename = 'logistic_reg_model.sav'
-#loaded_model = joblib.load(filename)
 
-
-option = st.sidebar.selectbox(
-    'Select the tennis player you would like to predict injury risk from',
-     unique_names)
-
-'You selected:', option
-
-'Starting computation...'
 
