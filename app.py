b8c1b7bea51b98135933fa7d913c00a4f68a6766
daniella-patton <59801897+daniella-patton@users.noreply.github.com>
2020-01-27 21:09:51 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,13 +5,26 @@ from sklearn.externals import joblib
 
 st.title('TennisDoctor')
 
-@st.cache
-def get_data():
-    url = "https://github.com/daniella-patton/Test2/blob/master/App_data.csv"
-    return pd.read_csv(url, error_bad_lines=False)
+if len(sys.argv) > 1: # parse command-line argument if supplied (folder path)
+    folder = os.path.abspath(sys.argv[1])
+else: # otherwise use the current working directory
+    folder = os.path.abspath(os.getcwd())
+ 
+st.write(folder)
 
+# function to load model
+@st.cache # cache
+def load_csv(modelName):
+	model = pd.read_csv(os.path.join(folder, modelName + '.csv'))
+	return model
+        
+#@st.cache
+#def get_data():
+#    url = "https://github.com/daniella-patton/Test2/blob/master/App_data.csv"
+#    return pd.read_csv(url, error_bad_lines=False)
 
-master = get_data()
+modelName = 'App_data'
+master = load_csv(modelName)
 st.write(master)
 #unique_names = master.PlayerName.unique()
 
