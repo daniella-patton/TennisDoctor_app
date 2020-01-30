1e3d3f77d82e2ba17d8cfad91eac9da1a338f6ce
daniella-patton <59801897+daniella-patton@users.noreply.github.com>
2020-01-30 18:20:28 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,17 @@ import numpy as np
 import pandas as pd
 from sklearn.externals import joblib
 
-st.title('TennisDoctor')
+
+"""
+# TennisDoctor
+
+This is a machine learning model that predicts the three month injury risk of female professional 
+tennis players.
+
+This model is based off of privious match, injury, and ranking history. 
+
+Risk options - Lower Risk/Higher Risk
+"""
 
 url = 'https://raw.githubusercontent.com/daniella-patton/Test2/master/Filter_Final_Logistic_Regression.csv'
 df = pd.read_csv(url, index_col=0)
@@ -11,13 +21,6 @@ df = pd.read_csv(url, index_col=0)
 st.write(df)
 unique_names = df.PlayerName.unique()
 
-"""
-# Predicting Risk of Injury in Female Professional Tennis Players
-
-This is a machine learning model that predicts the risk of player injury in the following three months.
-
-Risk options - Lower Risk/Higher Risk
-"""
 
 option = st.sidebar.selectbox(
     'Select the tennis player you would like to predict injury risk from',
