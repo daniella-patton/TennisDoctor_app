6fed22e42f1d603da613231ba87b574fb7151679
daniella-patton <59801897+daniella-patton@users.noreply.github.com>
2020-01-30 18:38:38 -0500

    Update app.py

--- app.py
+++ app.py
@@ -12,18 +12,15 @@ tennis players.
 
 This model is based off of privious match, injury, and ranking history. 
 
+Select the player of interest from the drop down menu on the left. 
+
 Risk options - Lower Risk/Higher Risk
 """
-
 url = 'https://raw.githubusercontent.com/daniella-patton/Test2/master/Filter_Final_Logistic_Regression.csv'
 df = pd.read_csv(url, index_col=0)
-
 unique_names = df.PlayerName.unique()
-
 option = st.sidebar.selectbox(
     'Select the tennis player you would like to predict injury risk from',
      unique_names)
-
 'You selected:', option
 
-'Starting computation...'
