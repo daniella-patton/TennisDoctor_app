f76e50812078d9d750eadbbe35464f090434368e
daniella-patton <59801897+daniella-patton@users.noreply.github.com>
2020-02-01 20:08:06 -0500

    Update app.py

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ name_filter = df[df['PlayerName'] == option]
 'Based off of your selection, we predict that ',  option, ' is'
 
 # load the model from disk
-loaded_model = pickle.load(open('https://github.com/daniella-patton/Test2/blob/master/random_forest_model.sav?raw=true', 'rb'))
+loaded_model = pickle.load(open('https://github.com/daniella-patton/Test2/blob/master/random_forest_model.sav', 'rb'))
 #result = loaded_model.score(X_test, Y_test)
 #print(result)
 
