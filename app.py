8c15e1e62cd6b67f115d04f66bd03e4395fb4b3b
daniella-patton <59801897+daniella-patton@users.noreply.github.com>
2020-02-04 09:19:46 -0500

    Update app.py

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ option = st.sidebar.selectbox(
      unique_names)
 'You selected:', 
 with st.echo():
-	write(option)
+	st.write(option)
 
 #name_df = pd.DataFrame([option], columns = ['Player Name']) 
 #st.dataframe(name_df)
