5d12402a9d4a25c95ef4d6d5a0b5f9b40019ec79
daniella-patton <59801897+daniella-patton@users.noreply.github.com>
2020-02-06 11:19:08 -0500

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ Risk options - Lower Risk/Higher Risk
 url = 'https://raw.githubusercontent.com/daniella-patton/Test2/master/Filter_Final_Logistic_Regression.csv'
 df = pd.read_csv(url, index_col=0)
 unique_names = df.PlayerName.unique()
-option = st.sidebar.selectbox('<span style="font-size:20pt; font-style:bold;"> Select the tennis player you would like to predict injury risk from',
+option = st.sidebar.selectbox('Select the tennis player you would like to predict injury risk from',
      unique_names)
 		
 'You selected:', 
