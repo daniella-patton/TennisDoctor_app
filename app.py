b66ec86683f900111bead2d5dc5383bf47c5281a
daniella-patton <59801897+daniella-patton@users.noreply.github.com>
2020-02-08 15:16:28 -0700

    Update app.py

--- app.py
+++ app.py
@@ -8,8 +8,56 @@ import os, sys, pickle
 from PIL import Image
 
 
+st.markdown('''
+<style>
+body {
+    background-color:#e6f9ff;
+}
+<style>
+    ''', unsafe_allow_html=True)
+
+# load the model from disk
+if len(sys.argv) > 1: # parse command-line argument if supplied (folder path)
+    folder = os.path.abspath(sys.argv[1])
+else: # otherwise use the current working directory
+    folder = os.path.abspath(os.getcwd())
+
+# function to load model
+def load_model(modelName):
+    model = pd.read_pickle(os.path.join(folder, modelName + '.sav'))
+    return model
+
+# function to load in csv
+def load_csv(csv_name):
+    player_csv = pd.read_csv(os.path.join(folder, csv_name + '.csv'), index_col=0)
+    return player_csv
+
+# function to load image
+def load_pic(option):
+    try:
+        file = os.path.join(folder, 'WTA_Pictures', option + '.jpg')
+        image = Image.open(file)
+    except IOError:
+        file = os.path.join(folder, 'WTA_Pictures', 'base.jpg') # base.jpg
+        image = Image.open(file)
+    return image
+
+# Load in csv
+csv_name = 'Current_Player_Info'
+df = load_csv(csv_name)
+unique_names = df.PlayerName.unique()
+
+
+# Load in Model
+modelName = 'logistic_model'
+model = load_model(modelName)
+
+
+# Main page layout
+
 st.title('TennisDoctor')
 st.header('An injury predictor for female professional tennis players.')
+
 
 """
 
@@ -21,9 +69,9 @@ tennis players based off of match, injury, and ranking history.
 Risk options - Lower Risk/Higher Risk
 """
 
-url = 'https://raw.githubusercontent.com/daniella-patton/Test2/master/Filter_Final_Logistic_Regression.csv'
-df = pd.read_csv(url, index_col=0)
-unique_names = df.PlayerName.unique()
+#url = 'https://raw.githubusercontent.com/daniella-patton/Test2/master/Filter_Final_Logistic_Regression.csv'
+#df = pd.read_csv(url, index_col=0)
+#unique_names = df.PlayerName.unique()
 option = st.sidebar.selectbox('Select the tennis player',
      unique_names)
 		
@@ -40,58 +88,24 @@ name_filter = df[df['PlayerName'] == option]
 
 'We predict that ',  option, ' is:'
 
-# Running the machine learning model on the data
+# Running the logistic regression model on the data
+result = model.predict_proba(name_filter)
 
-# load the model from disk
-if len(sys.argv) > 1: # parse command-line argument if supplied (folder path)
-    folder = os.path.abspath(sys.argv[1])
-else: # otherwise use the current working directory
-    folder = os.path.abspath(os.getcwd())
+results_p = result[:,1]
 
-# function to load model
-def load_model(modelName):
-	model = pd.read_pickle(os.path.join(folder, modelName + '.sav'))
-	return model
+if results_p >= 0.75:
+    risk_as = 'At higher risk injury'
 
-# load model
-modelName = 'logistic_model'
-model = load_model(modelName)
+if results_p < 0.75 and results_p >= 0.5:
+    risk_as = 'At moderate risk of injury'
 
-#result = loaded_model.score(X_test, Y_test)
+if results_p < 0.5:
+    risk_as = 'At low risk of injury'
+
 #print(result)
-
+#risk_r = 'At low risk'
+st.markdown('**' + risk_as + '**')
 risk_r = 'At low risk'
-
-# st.markdown(**' + str(risk_r) + '**')
-#st.markdown('<span style="font-size:36pt; 
-#	    font-style:bold;"> str(risk_r)</span><br><span style="font-size:24pt; font-style:italic;
-#	    ">Find your next page turner</span>', unsafe_allow_html=True)
-
-st.markdown('<span style="font-size:30pt; 
-	    font-style:bold;"> str(risk_r)</span>', unsafe_allow_html=True)
-
-
-# LOADING IN PICTURE
-def load_pic(option):
-	try:
-		file = os.path.join(folder, 'WTA_Pictures', option + '.jpg')
-		image = Image.open(file)
-	except IOError:
-		file = os.path.join(folder, 'WTA_Pictures', 'base.jpg') # base.jpg
-		image = Image.open(file)
-	return image
-
-player_pic = load_pic(option)
-  
-#else:
-#    image = Image.open('base.jpg')
-st.image(player_pic, caption = option, width=None)
-
-# risk_df = pd.DataFrame([risk_r, st.image(player_pic, caption = option, width=None)], columns = ['Risk', str(option)]) 
-risk_df = pd.DataFrame([risk_r, st.image(player_pic, caption = option, width=None)]) 
-
-
-st.dataframe(risk_df)
 
 c = alt.Chart(name_filter, width=800,
     height=600, title="Matches Played and Injury History").mark_bar().encode(
